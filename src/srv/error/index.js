1c26c26e63abc9e6a969575077b440e515c73fb8
AlejandroHerr <alejandrohnc88@gmail.com>
2017-05-30 18:45:46 +0200

    HttpStatus

---
+++ src/srv/error/index.js
@@ -0,0 +1,24 @@
+const errorBadRequest = new Error('Not Found');
+errorBadRequest.status = 404;
+
+export const badRequest = errorBadRequest;
+
+const errorNotFound = new Error('Not Found');
+errorNotFound.status = 404;
+
+export const notFound = errorNotFound;
+
+const errorMethodNotAllowed = new Error('Method Not Allowed');
+errorMethodNotAllowed.status = 405;
+
+export const methodNotAllowed = errorMethodNotAllowed;
+
+const errorNotAcceptable = new Error('Not Acceptable');
+errorNotAcceptable.status = 406;
+
+export const notAcceptable = errorNotAcceptable;
+
+const errorUnsupportedMediaType = new Error('Unsupported Media Type');
+errorUnsupportedMediaType.status = 415;
+
+export const unsupportedMediaType = errorUnsupportedMediaType;
