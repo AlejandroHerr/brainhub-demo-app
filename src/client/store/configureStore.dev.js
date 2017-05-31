8964b682e6091ac89af9735fd0134b92b1300dcf
AlejandroHerr <alejandrohnc88@gmail.com>
2017-05-31 03:30:54 +0200

    Fix logger

--- src/client/store/configureStore.dev.js
+++ src/client/store/configureStore.dev.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { logger } from 'redux-logger';
+import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
@@ -14,7 +14,6 @@ const configureStore = (preloadedState) => {
   );
 
   if (module.hot && process.env.NODE_ENV !== 'production') {
-    console.log('heye');
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
       const nextRootReducer = require('../reducers').default;
