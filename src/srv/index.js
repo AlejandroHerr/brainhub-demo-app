2303fd59e76d80c258855004ebf797af092764d0
AlejandroHerr <alejandrohnc88@gmail.com>
2017-06-03 18:46:39 +0200

    Mongodb

--- src/srv/index.js
+++ src/srv/index.js
@@ -1,5 +1,14 @@
+import { MongoClient } from 'mongodb';
 import server from './server';
 
-server.listen(process.env.PORT, process.env.ADDR, () => {
-  console.log(`Brainhub demo app listening on port ${process.env.ADDR}:${process.env.PORT}!`);
+console.log(`mongodb://${process.env.MONGO_ADDR}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`);
+MongoClient.connect(`mongodb://${process.env.MONGO_ADDR}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`, (err, database) => {
+  if (err) throw err;
+
+  const db = database;
+
+  server.listen(process.env.PORT, process.env.ADDR, () => {
+    server.db = db;
+    console.log(`Brainhub demo app listening on port ${process.env.ADDR}:${process.env.PORT}!`);
+  });
 });
