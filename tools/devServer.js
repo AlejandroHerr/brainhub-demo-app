d5a60f24e45d95c7d2f3d792a400a52e81be5d0e
AlejandroHerr <alejandrohnc88@gmail.com>
2017-06-03 21:47:55 +0200

    Mongo in devserver

--- tools/devServer.js
+++ tools/devServer.js
@@ -1,3 +1,4 @@
+import { MongoClient } from 'mongodb';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
@@ -8,10 +9,16 @@ const compiler = webpack(config);
 server.use(webpackDevMiddleware(compiler, { stats: { colors: true }, noInfo: false, publicPath: '/' }));
 server.use(webpackHotMiddleware(compiler));
 
-server.listen(process.env.PORT, process.env.ADDR, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Brainhub demo app listening on port ${process.env.ADDR}:${process.env.PORT}!`);
-  }
+MongoClient.connect(`mongodb://${process.env.MONGO_ADDR}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`, (err, database) => {
+  if (err) throw err;
+
+  const db = database;
+  server.db = db;
+  server.listen(process.env.PORT, process.env.ADDR, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Brainhub demo app listening on port ${process.env.ADDR}:${process.env.PORT}!`);
+    }
+  });
 });
