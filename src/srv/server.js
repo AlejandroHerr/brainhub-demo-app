71435adc9b5b2789c4da720a69df414bf32e8ad1
AlejandroHerr <alejandrohnc88@gmail.com>
2017-06-03 21:55:05 +0200

    Serve static

--- src/srv/server.js
+++ src/srv/server.js
@@ -4,6 +4,8 @@ import api from './router/api';
 const server = express();
 
 server.use('/api', api);
+server.use(express.static('public'));
+
 if (process.env.NODE_ENV !== 'development') {
   server.get('*', (req, res) => {
     res.sendFile('index.html', { root: `${__dirname}/../../public/` });
