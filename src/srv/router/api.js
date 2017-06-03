8d0a46ae06dae1bbb19213a2f498081132f206d7
AlejandroHerr <alejandrohnc88@gmail.com>
2017-06-03 21:03:03 +0200

    Mongodb

--- src/srv/router/api.js
+++ src/srv/router/api.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { json } from 'body-parser';
-import * as error from '../error';
+import * as httpError from '../error';
 import checkJsonHeaders from '../middleware/checkJsonHeaders';
 import hasBody from '../middleware/hasBody';
 import validatorCreator from '../middleware/validator';
@@ -11,17 +11,25 @@ const router = Router();
 router.all('/attendant', (req, res, next) => {
   if (req.method === 'POST') return next();
 
-  return next(error.methodNotAllowed);
+  return next(httpError.methodNotAllowed);
 });
 
 const jsonParser = json();
 const validator = validatorCreator(formOptions);
 
-router.post('/attendant', checkJsonHeaders, jsonParser, hasBody, validator, (req, res) => {
-  res.status(201).json({ message: 'Attendant created.' });
+router.post('/attendant', checkJsonHeaders, jsonParser, hasBody, validator, (req, res, next) => {
+  const { app: { db }, body } = req;
+
+  db.collection('attendants').insert(body, (err) => {
+    if (err) {
+      return next(httpError.internalServerError);
+    }
+
+    return res.status(201).json({ message: 'Attendant created.' });
+  });
 });
 
-router.all('*', (req, res, next) => next(error.notFound));
+router.all('*', (req, res, next) => next(httpError.notFound));
 
 
 router.use((err, req, res, next) => res
