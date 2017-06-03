8d22bd0cc1fcbe56a8b08a13c07778a9b000a052
AlejandroHerr <alejandrohnc88@gmail.com>
2017-06-03 21:05:17 +0200

    Refactor

--- src/srv/middleware/validator.js
+++ src/srv/middleware/validator.js
@@ -1,27 +1,31 @@
 import every from 'lodash/every';
 import filter from 'lodash/filter';
-import find from 'lodash/find';
 import get from 'lodash/get';
 import keysIn from 'lodash/keysIn';
-import mapValues from 'lodash/mapValues';
+import map from 'lodash/map';
 import size from 'lodash/size';
 import some from 'lodash/some';
 import defaultValidator from '../../common/validator';
 import { badRequest } from '../error';
 
 const validate = (data, fields, validator) => {
-  const validation = mapValues(data, (value, field) => {
-    const validators = get(find(fields, ['name', field]), 'validators', []);
-    return validator(value, validators);
+  const validation = map(fields, (field) => {
+    const result = validator(get(data, field.name, null), field.validators);
+    return {
+      name: field.name,
+      ...result,
+    };
   });
   return filter(validation, ['valid', false]);
 };
+
 const isValid = validation => size(validation) === 0;
 
 export default (formOptions, validator = defaultValidator) => (req, res, next) => {
   const { body } = req;
   const { fields } = formOptions;
 
+  // Checks if there's fields strange to the form
   if (!every(keysIn(body), field => some(fields, ['name', field]))) {
     return next(badRequest);
   }
