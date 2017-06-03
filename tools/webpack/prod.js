768976392093c7ede6f15e16a7e8b91a51d0f306
AlejandroHerr <alejandrohnc88@gmail.com>
2017-06-03 21:55:32 +0200

    Production script updated

--- tools/webpack/prod.js
+++ tools/webpack/prod.js
@@ -1,36 +1,34 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 const baseConfig = require('./base.js');
 
-module.exports = webpackMerge(baseConfig,
-  {
-    entry:
-    './src/index.js',
-
-    devtool: 'source-map',
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false,
-      }),
-
-      new webpack.optimize.UglifyJsPlugin({
-        beautify: false,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true,
-        },
-        compress: {
-          screw_ie8: true,
-        },
-        comments: false,
-        sourceMap: true,
-      }),
-    ],
-  });
+module.exports = webpackMerge(baseConfig, {
+  entry: path.resolve(__dirname, '../../', 'src', 'client', 'index.js'),
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true,
+      },
+      compress: {
+        screw_ie8: true,
+      },
+      comments: false,
+      sourceMap: true,
+    }),
+    new ExtractTextPlugin('main.css'),
+  ],
+});
